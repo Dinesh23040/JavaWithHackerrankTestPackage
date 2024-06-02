ee1b31efead6dd46bac3663f7f5f062a9e1fffc0
dineshcloudone <dinesh.cloudone@gmail.com>
2024-06-02 18:09:38 +0530

    first commit
    Signed-off-by: dineshcloudone <dinesh.cloudone@gmail.com>

---
+++ src/ScannerPractice.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class ScannerPractice {
+
+public static void main(String[] args) {		
+	
+//	  System.out.print("Enter your name : ");//	  
+//	  Scanner s=new Scanner(System.in);//	  
+//	  String name=s.nextLine();//	  
+//	  System.out.println("Data gathered through system class : "+ name);	
+	
+	Scanner scan = new Scanner(System.in);
+    int i = scan.nextInt();
+   double d=scan.nextDouble();
+   scan.nextLine();
+   String s=scan.nextLine();
+   
+   System.out.println("String: " + s);
+   System.out.println("Double: " + d);
+   System.out.println("Int: " + i);
+	 
+}
+
+}
+
+
